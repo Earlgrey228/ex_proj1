a6c229a68b6561e0439871eab5eedac3dc1b5145
EarlGray <msyyer@bk.ru>
2023-09-08 16:06:46 +0300

    Добавлены бесполезные файлы, нормальные добавлю после выдачи стипухи

---
+++ jws/HelloITIS.java
@@ -0,0 +1,5 @@
+class HelloITIS {
+    public static void main(String[] args) {
+        System.out.println("Hello ITIS!!!"); 
+    }
+}
---
+++ jws/HelloandByeWorld.java
@@ -0,0 +1,8 @@
+public class HelloandByeWorld {
+   
+    public static void main(String[] args) {
+           
+        System.out.println("Hello world!");
+        System.out.println("Bye world...");
+    }
+}
