6c67a60ca4731cc4dd3973f001c1bb62e5b82c68
EarlGray <msyyer@bk.ru>
2023-09-08 16:10:19 +0300

    Удалил некоторые ненужные файлы p.s.нужных нет ;)

--- jws/HelloandByeWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloandByeWorld {
-   
-    public static void main(String[] args) {
-           
-        System.out.println("Hello world!");
-        System.out.println("Bye world...");
-    }
-}
